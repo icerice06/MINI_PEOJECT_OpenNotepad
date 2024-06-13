fdfe2e14a4f03501e9635f8a5e6f901c888f2e8d
Chanbin Jeong <bin37436899@gmail.com>
2024-06-13 15:21:00 +0900

    Delete back_end directory

--- back_end/src/main/java/com/icerice/back_end/repository/NotepadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.icerice.back_end.repository;
-
-import com.icerice.back_end.entity.NotepadEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotepadRepository extends JpaRepository<NotepadEntity, Long> {
-}
